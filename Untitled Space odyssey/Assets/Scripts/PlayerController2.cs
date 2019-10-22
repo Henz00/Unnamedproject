81a355cfb51662c91739216252af3ea843928bd9
TheNoblish <DrNoble2.0@gmail.com>
2019-10-22 12:23:58 +0200

    Oprettlse af unity

---
+++ Untitled Space odyssey/Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed = 30;
+    public bool jump = false;
+    public bool grounded = true;
+    public float maxspeed = 10;
+    public float jumpspeed = 10;
+    public Transform groundCheck;
+    public LayerMask mask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D rh = Physics2D.Linecast(transform.position, groundCheck.position, mask);
+        if (rh.collider != null)
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            jump = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal 2");
+        rb.AddForce(h * Vector2.right * speed);
+
+        if (Mathf.Abs(rb.velocity.x) > maxspeed)
+        {
+            float sign = Mathf.Sign(rb.velocity.x);
+            rb.velocity = new Vector2(sign, rb.velocity.y);
+        }
+
+        if (jump && grounded)
+        {
+
+            rb.AddForce(Vector2.up * jumpspeed, ForceMode2D.Impulse);
+            jump = false;
+        }
+    }
+}
