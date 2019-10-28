67137c991e9b3b5cfd057bbbc4d3c717c99031a9
Mimersbrønd <sten-.-@hotmail.com>
2019-10-28 19:35:03 +0100

    Main og Pause Menu. Restart virker ikke og Load Game ikke lavet endnu

---
+++ Untitled Space odyssey/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+        Debug.Log("LoadScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
