bb6ea7b0f534e9c9ec2245f918d0366a694a40ef
TheNoblish <DrNoble2.0@gmail.com>
2019-11-01 00:07:40 +0100

    Extension of test level

---
+++ Untitled Space odyssey/Assets/Scripts/OutOfBounds.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OutOfBounds : MonoBehaviour
+
+{
+    
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "Player 1" || other.gameObject.name == "Player 2")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            //Debug.Log("hit");
+        }
+    }
+}
