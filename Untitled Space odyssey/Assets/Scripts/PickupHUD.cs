07cfe363536fde5d568d6769c07bc12e98aef6d2
Henz <henha18@student.sdu.dk>
2019-11-06 15:55:09 +0100

    Auto stash before merge of "master" and "origin/master"

--- Untitled Space odyssey/Assets/Scripts/PickupHUD.cs
+++ Untitled Space odyssey/Assets/Scripts/PickupHUD.cs
@@ -8,9 +8,14 @@ public class PickupHUD : MonoBehaviour
     public Slider m_PickUpSlider;
     public float datascore = 0;
 
+    private ArrayList datalist;
+
     void Start()
     {
         m_PickUpSlider.value= 0F;
+        //m_PickUpSlider.maxValue = ;
+
+       
     }
 
 
