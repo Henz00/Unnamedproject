b42ef59fe6da15e68f9166a35cc82fd29ee35c37
TheNoblish <DrNoble2.0@gmail.com>
2019-11-13 15:43:33 +0100

    Intro scene + dialogue
    Creation and implementation of introductory sequence + creation of dialogue system

---
+++ Untitled Space odyssey/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public GameObject manager;
+    public void Start()
+    {
+       
+    }
+    public void triggerDialogue()
+    {
+        manager.GetComponent<dialogueManager>().StartDialogue(dialogue);
+    }
+}
--- Untitled Space odyssey/Assets/Scripts/MainMenu.cs
+++ Untitled Space odyssey/Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
         Debug.Log("LoadScene");
     }
 
