dfbe47d078b8626f1559125617c2624362d75556
Henz <henha18@student.sdu.dk>
2019-11-13 14:11:28 +0100

    Slow motion
    Slow motion to 70% by pressing Q

---
+++ Untitled Space odyssey/Assets/Scripts/SlowMotion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowMotion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            if (Time.timeScale == 1.0f)
+                Time.timeScale = 0.7f;
+            else
+                Time.timeScale = 1f;
+        }
+            
+    }
+}
