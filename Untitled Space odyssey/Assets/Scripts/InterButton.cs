b5286e49dff3536570aa029c9a39c718e740689c
Henrik <henha18@student.sdu.dk>
2019-11-15 14:59:51 +0100

    Buttonscript + prefab

---
+++ Untitled Space odyssey/Assets/Scripts/InterButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterButton : MonoBehaviour
+{
+    private static bool interactable = false;
+    private bool activate = false;
+
+
+    void Update()
+    {
+        if (interactable)
+        {
+            if (Input.GetKeyDown("e"))
+            {
+                if (!activate)
+                {
+                    Debug.Log("Activated button!");
+                    activate = true;
+
+                }
+                else
+                {
+                    Debug.Log("Deactivated button!");
+                    activate = false;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        interactable = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        interactable = false;
+    }
+}
