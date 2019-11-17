b1f1a6a105e93f3a174b6052435797599f7f30e6
Mikkel <wemikjak@gmail.com>
2019-11-17 12:17:32 +0100

    Area name Fade in/out
    Tilføjet område navn prefab, som man bare kan sætte ind, og ændre på texten

---
+++ Untitled Space odyssey/Assets/textTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textTrigger : MonoBehaviour
+{
+    public Canvas AreaText;
+    public Animator TextAnimator;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AreaText.enabled = false;
+        TextAnimator.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Palyer 1" || other.tag == "Player 2")
+        {
+            TextAnimator.enabled = true;
+            AreaText.enabled = true;
+        }
+    }
+}
