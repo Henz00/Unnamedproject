bf081a4ac89853ae24ae8b02d2a1643414517bf6
Mimersbrønd <sten-.-@hotmail.com>
2019-11-18 20:59:39 +0100

    taking back my tutorialscreen

---
+++ Untitled Space odyssey/Assets/Scripts/TutorialScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class TutorialScreen : MonoBehaviour
+{
+    public GameObject Player1;
+    public GameObject Player2;
+    public GameObject Other;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player1.SetActive(true);
+        Player2.SetActive(false);
+        Other.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
+
