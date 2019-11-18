c040fd8c6e4955c251817f4d25f34bc4a702e6dd
Henz <henha18@student.sdu.dk>
2019-11-18 16:55:25 +0100

    Puzzle door with lever (interchangeable with button)
    With minor changes to the prefab it can be made to work with a button aswell

---
+++ Untitled Space odyssey/Assets/Scripts/puzzleDoor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puzzleDoor : MonoBehaviour
+{
+    private BoxCollider2D kasse;
+    private bool activated;
+
+    void Start()
+    {
+        kasse = gameObject.GetComponentInChildren<BoxCollider2D>();
+    }
+    void Update()
+    {
+        activated = gameObject.GetComponentInChildren<Interactable>().activate;
+
+        if (activated) 
+        {
+            kasse.enabled = false;
+        }  
+        else
+        {
+            kasse.enabled = true;
+        }        
+    }
+}
