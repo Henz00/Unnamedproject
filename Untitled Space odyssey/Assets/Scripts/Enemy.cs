2a61bf236ecd151fa93805aa7b58f08afbfe7063
TheNoblish <DrNoble2.0@gmail.com>
2019-12-03 02:02:44 +0100

    Double squish + "squish" sound

--- Untitled Space odyssey/Assets/Scripts/Enemy.cs
+++ Untitled Space odyssey/Assets/Scripts/Enemy.cs
@@ -6,20 +6,49 @@ public class Enemy : MonoBehaviour
 {
     public LayerMask layerMask;
     public LayerMask PlayerMask;
+    public LayerMask Player1Mask;
+    public LayerMask Player2Mask;
+    public LayerMask SquishChecker;
     public int move = 1;
     public float speed;
     public bool canMove = true;
+    public GameObject attack1;
+    public GameObject attack2;
+    public AudioSource squisher;
 
+    private Collider2D col;
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate ()
     {
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(transform.position, new Vector2(1.9f, 1f), 0f, new Vector2(0, 1), distance: 1F, PlayerMask);
+
+
+        if (col.IsTouchingLayers(Player1Mask) && col.IsTouchingLayers(Player2Mask))
+        {
+            Debug.Log("squish");
+            Invoke("death", 1f);
+        }
+        else if (raycastHit2D.collider != null)
+        {
+            if (raycastHit2D.collider.CompareTag("Player 1"))
+                SquishChecker = Player2Mask;
+            else if (raycastHit2D.collider.CompareTag("Player 2"))
+                SquishChecker = Player1Mask;
+            if (PlayerCheck(SquishChecker, raycastHit2D.transform))
+            {
+                Debug.Log("squish");
+                death();
+            }
+        }
+
 
         if (PlayerCheck(PlayerMask, transform))
         {
@@ -42,6 +71,15 @@ public class Enemy : MonoBehaviour
 
     }
 
+    private void death()
+    {
+        squisher.Play();
+        col.enabled = false;
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        attack1.SetActive(false);
+        attack2.SetActive(false);
+        //gameObject.SetActive(false);
+    }
 
     private bool boxCheckRight(LayerMask layerMask)
     {
