9e1734d0f8264ef7684876d932cee4dc26ccd287
TheNoblish <DrNoble2.0@gmail.com>
2019-12-05 02:44:29 +0100

    musicmanager

---
+++ Untitled Space odyssey/Assets/Scripts/musicManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class musicManager : MonoBehaviour
+{
+    private bool level = false;
+    private AudioSource source;
+
+    public AudioClip levelClip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        source = gameObject.GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByBuildIndex(3) && level == false)
+        {
+            level = true;
+            source.clip = levelClip;
+            source.Play();
+        }
+    }
+}
