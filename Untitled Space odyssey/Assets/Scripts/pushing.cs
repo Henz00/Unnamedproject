d991888e3dd21542c2e1b90fb05e0e101a8d96de
TheNoblish <DrNoble2.0@gmail.com>
2019-12-05 02:13:43 +0100

    push boulder sprite, push particles, beam particles

---
+++ Untitled Space odyssey/Assets/Scripts/pushing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pushing : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public ParticleSystem dust;
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb.velocity.x > 0)
+        {
+            dust.Play();
+        }
+    }
+}
