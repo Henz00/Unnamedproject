56b92567d201e71f4d065d14bbcde7137b9748a2
Henz <henha18@student.sdu.dk>
2019-12-20 21:55:06 +0100

    Final V1
    Ready to build and play

--- Untitled Space odyssey/Assets/Scripts/EndGame.cs
+++ Untitled Space odyssey/Assets/Scripts/EndGame.cs
@@ -1,17 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndGame : MonoBehaviour
 {
     private EndLevelFade endlevelfade;
+
+    void Start()
+    {
+        endlevelfade = GetComponent<EndLevelFade>();
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player 1") || other.CompareTag("Player 2"))
         {
             endlevelfade.camerashake();
+            Invoke("ChangeToEndScene", 2);
         }
+    }
+
+    void ChangeToEndScene()
+    {
+        SceneManager.LoadScene(4);
     }
   
 }
--- Untitled Space odyssey/Assets/Scripts/OutOfBounds.cs
+++ Untitled Space odyssey/Assets/Scripts/OutOfBounds.cs
@@ -8,7 +8,7 @@ public class OutOfBounds : MonoBehaviour
 {
     
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.name == "Player 1" || other.gameObject.name == "Player 2")
         {
